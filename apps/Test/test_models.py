70bf4f72fbe6973b54b76f7bcab0482fde37d61a
Byron <73563578+bountrix@users.noreply.github.com>
2020-11-16 00:39:23 -0300

    Implementando Test

---
+++ apps/Test/test_models.py
@@ -0,0 +1,26 @@
+from django.test import testcases
+
+# Create your tests here.
+
+class YourTestClass(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set non-modified data for all class methods.")
+        pass
+
+    def setUp(self):
+        print("SetUp: Run for every test method to setup clean data.")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):    
+        print("Method: test_false_is_true.")   
+        self.assertTrue(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
